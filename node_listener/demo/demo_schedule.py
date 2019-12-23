112593825718badd51bebddf5573a93788f12cd5
Bartosz Kościów <kosci1@gmail.com>
2019-12-23 13:05:17 +0100

    scheduler

--- node_listener/demo/demo_schedule.py
+++ node_listener/demo/demo_schedule.py
@@ -1,7 +1,8 @@
 from pprint import pprint
 from node_listener.worker.openweather_worker import OpenweatherWorker
-
 from configparser import ConfigParser
+import schedule
+import time
 
 config = ConfigParser()
 config.read("../../config.ini")
@@ -10,4 +11,20 @@ apikey = config["openweather"]["apikey"]
 cities = {3103402: "Bielsko-Biała"}
 
 w = OpenweatherWorker(cities, apikey)
-pprint(w.execute())
+# pprint(w.execute())
+
+class Bag(object):
+    def __init__(self, func):
+        self.func = func
+
+    def execute(self):
+        pprint(self.func())
+
+
+bag = Bag(w.execute)
+
+schedule.every(15).seconds.do(bag.execute)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
