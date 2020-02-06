8af77627b46f4f46c5fca0765a63985316d18d31
Bartosz Kościów <kosci1@gmail.com>
2020-02-06 20:55:08 +0100

    fix tests

--- node_listener/tests/test_server.py
+++ node_listener/tests/test_server.py
@@ -17,12 +17,17 @@ class TestDictionaryEngine(object):
     def setUp(self):
         Storage.set_engine(MagicMock())
         self.storage = Storage()
-        self.server = SensorListener(self.storage)
+        self.config = MagicMock()
+        self.config.get_dict.return_value = {'1': 'BB'}
+        self.server = SensorListener(self.storage, self.config)
 
     def test_node_one_should_be_added(self):
         assert_is_instance(self.server.svr.handlers['NodeOne'][0], NodeOneHandler)
 
     def test_start_should_start_server(self):
+
         self.server.svr = MagicMock()
+        self.server.executor = MagicMock()
         self.server.start()
         self.server.svr.start.should_be_called()
+        self.server.executor.start.should_be_called()
