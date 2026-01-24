34ac214997d95406afb5c7a5037248101d2867f0
bkosciow <kosci1@gmail.com>
2026-01-24 21:25:01 +0100

    command for manging models

--- nextcloud_bot.py
+++ nextcloud_bot.py
@@ -11,7 +11,13 @@ from datetime import datetime
 from nc_bot.air import air_quality
 from nc_bot.weather import weather
 from nc_bot.home import home, room
+import ollama
 
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(name)s %(message)s'
+)
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
@@ -27,6 +33,7 @@ patterns = [
     '!weather',
     '!home',
     '!home <room>',
+    '?rem <action>',
     '?rem <action> <object>',
     '!rem <prompt>',
     'Rem <prompt>'
@@ -38,45 +45,63 @@ initialized = False
 
 async def action(request):
     global initialized
-    if not initialized:
-        await assistant.load_mcps(config.get_list('assistant.mcp_servers'))
-        initialized = True
+    try:
+        if not initialized:
+            await assistant.load_mcps(config.get_list('assistant.mcp_servers'))
+            initialized = True
 
-    cmd = request.parse_command(patterns)
-    # print(cmd.result, cmd.command)
-    if cmd.result:
-        if cmd.command == '!sl':
-            if cmd.action == "keys":
-                data = storage.get_all()
-                request.reply("\n".join(data.keys()))
+        cmd = request.parse_command(patterns)
+        # print(cmd.result, cmd.command)
+        if cmd.result:
+            if cmd.command == '!sl':
+                if cmd.action == "keys":
+                    data = storage.get_all()
+                    request.reply("\n".join(data.keys()))
 
-            if cmd.action == "data":
-                data = storage.get(cmd.module)
-                request.post(json.dumps(data))
+                if cmd.action == "data":
+                    data = storage.get(cmd.module)
+                    request.post(json.dumps(data))
 
-        if cmd.command == "!air":
-            request.post(air_quality(storage.get("openaq")))
+            if cmd.command == "!air":
+                request.post(air_quality(storage.get("openaq")))
 
-        if cmd.command == "!weather":
-            request.post(weather(storage.get('openweather')))
+            if cmd.command == "!weather":
+                request.post(weather(storage.get('openweather')))
 
-        if cmd.command == "!home":
-            if cmd.room is not None:
-                request.post(room(cmd.room))
-            else:
-                request.post(home(storage))
-        if cmd.command == '?rem':
-            if cmd.action == 'reload':
-                if cmd.object == 'mcp':
-                    await assistant.clear_mcps()
-                    await assistant.load_mcps(assistant.mcps)
-                    request.reply("Tools reloaded")
+            if cmd.command == "!home":
+                if cmd.room is not None:
+                    request.post(room(cmd.room))
+                else:
+                    request.post(home(storage))
+            if cmd.command == '?rem':
+                if cmd.action == 'model' and cmd.object is None:
+                    request.reply(assistant.model)
+                elif cmd.action == 'model':
+                    assistant.model = cmd.object
+                    request.reply(f"model changed to {cmd.object}")
 
-        if cmd.command == '!rem' or cmd.command == 'Rem':
-            #request.reply("...")
-            response = await assistant.query(cmd.text)
-            # print(response)
-            request.post(response.message.content)
+                if cmd.action == 'list':
+                    if cmd.object == 'models':
+                        models = await assistant.get_models()
+                        text = "| name | size | parameter_size | quantization_level |\n"
+                        text += "|------|------|----------------|--------------------|\n"
+                        for model in models:
+                            text += f"| {model.model} | {model.size//1024//1024//1024} GB | {model.details.parameter_size} | {model.details.quantization_level} |\n"
+                        request.reply(text)
+
+                if cmd.action == 'reload':
+                    if cmd.object == 'mcp':
+                        await assistant.clear_mcps()
+                        await assistant.load_mcps(assistant.mcps)
+                        request.reply("Tools reloaded")
+
+            if cmd.command == '!rem' or cmd.command == 'Rem':
+                response = await assistant.query(cmd.text)
+                request.post(response.message.content)
+
+    except (ollama.ResponseError, ConnectionError) as e:
+        logger.error(e)
+        request.reply(str(e))
 
 
 init_server(
